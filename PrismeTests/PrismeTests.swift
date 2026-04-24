9527026e436a7b3b7d891a5d8d4a4a1c7b2f699f
Louise Leroux <louiseleroux1992@gmail.com>
2026-04-24 16:50:23 +0200

    Initial Commit

---
+++ PrismeTests/PrismeTests.swift
@@ -0,0 +1,16 @@
+//
+//  PrismeTests.swift
+//  PrismeTests
+//
+//  Created by Louise Leroux on 24/04/2026.
+//
+
+import Testing
+
+struct PrismeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
